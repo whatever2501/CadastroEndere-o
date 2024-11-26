1cde4fa551620f1ea910823e7de439afe8a6d3a1
whatever2501 <luiz_nando_vieira@hotmail.com>
2024-11-26 19:37:01 -0300

    Enviando pastas

---
+++ Backend/src/main/java/com/CadastroEndereco/CadastroEnderecoApplication.java
@@ -0,0 +1,13 @@
+package com.CadastroEndereco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class  CadastroEnderecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroEnderecoApplication.class, args);
+	}
+
+}
