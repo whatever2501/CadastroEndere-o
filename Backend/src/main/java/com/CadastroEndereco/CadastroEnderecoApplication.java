a9d1679cc99584b0bc4bb6dbcc180e460b8e7741
whatever2501 <luiz_nando_vieira@hotmail.com>
2024-12-17 15:37:14 -0300

    teste

--- Backend/src/main/java/com/CadastroEndereco/CadastroEnderecoApplication.java
+++ Backend/src/main/java/com/CadastroEndereco/CadastroEnderecoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class  CadastroEnderecoApplication {
-
+ int x;	
 	public static void main(String[] args) {
 		SpringApplication.run(CadastroEnderecoApplication.class, args);
 	}
